859a430de7a45f52fc10705622677bcd911e59b6
Harsh Prakash <prakashharsh32@gmail.com>
2022-08-23 00:28:24 +0530

    mainnet version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # wallet-tracker-bot
- Telegram notification for incoming transactions on Polygon.
+Telegram notification for incoming transactions on Polygon.
+
+Try It Out At:
+
+```https://t.me/WebThreeBot```
+
+## Installation
+
+```bash
+npm install
+```
+Create a `.env` file with the following contents:
+```
+WS_RPC=<polygon-websocket-rpc>
+TELEGRAM_BOT_TOKEN=<your-telegram-bot-token>
+```
+Then run:
+```bash
+node tracker.js
+```
+
+Deployed on AWS 🔥
--- tracker.js
+++ tracker.js
@@ -2,21 +2,40 @@ require("dotenv").config();
 const axios = require("axios").default;
 const Web3 = require("web3");
 const Datastore = require("nedb");
+const { Telegraf } = require("telegraf");
 const db = new Datastore({ filename: "users.db", autoload: true });
 
-const web3 = new Web3(process.env.TEST_WS_RPC);
+const web3 = new Web3(process.env.WS_RPC);
+
+// const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
+const bot = new Telegraf("5594675918:AAHkK59ihuLzE65uY9XnKTBlCKmZtEuAwpg");
+
+bot.start(ctx => ctx.reply("Welcome, please enter your wallet address"));
+bot.on("text", ctx => {
+    console.log(ctx.update.message.from.id);
+    if (web3.utils.isAddress(ctx.update.message.text)) {
+        db.update(
+            { telegramId: ctx.update.message.from.id },
+            { wallet: ctx.update.message.text.toLowerCase(), telegramId: ctx.update.message.from.id },
+            { upsert: true }
+        );
+        ctx.reply("Thank you, your wallet address is saved and being monitored on polygon blockchain");
+    }
+});
+bot.launch();
+console.log("Bot started");
 
 // const tokenAddresses = ["0xd738179146f354f1ee576f449eca6f7cb4ece210"];
-const tokenLookUp = { "0xd738179146f354f1ee576f449eca6f7cb4ece210": "USDC" };
-// const tokenLookUp = {
-//     "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": "USDC",
-//     "0xc2132d05d31c914a87c6611c10748aeb04b58e8f": "USDT",
-//     "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063": "DAI",
-//     "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619": "WETH",
-//     "0x45c32fa6df82ead1e2ef74d17b76547eddfaff89": "FRAX",
-//     "0x2e1ad108ff1d8c782fcbbb89aad783ac49586756": "TUSD",
-//     "0xa3fa99a148fa48d14ed51d610c367c61876997f1": "MAI",
-// };
+// const tokenLookUp = { "0xd738179146f354f1ee576f449eca6f7cb4ece210": "USDC" };
+const tokenLookUp = {
+    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174": "USDC",
+    "0xc2132d05d31c914a87c6611c10748aeb04b58e8f": "USDT",
+    "0x8f3cf7ad23cd3cadbd9735aff958023239c6a063": "DAI",
+    "0x7ceb23fd6bc0add59e62ac25578270cff1b9f619": "WETH",
+    "0x45c32fa6df82ead1e2ef74d17b76547eddfaff89": "FRAX",
+    "0x2e1ad108ff1d8c782fcbbb89aad783ac49586756": "TUSD",
+    "0xa3fa99a148fa48d14ed51d610c367c61876997f1": "MAI",
+};
 
 const subscription = web3.eth
     .subscribe(
@@ -55,7 +74,7 @@ const subscription = web3.eth
                     .get(
                         `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${doc.telegramId}&parse_mode=MarkdownV2&text=*Credit*%0A%0ASender%20%3A%20${sender}%0A%0A*%5C%2B${amountText}%20${tokenLookUp[token]}*%0A%5BPolygonScan%5D(https%3A%2F%2Fpolygonscan.com%2Ftx%2F${tx})`
                     )
-                    .then(res => console.log("Result :", res.data))
+                    // .then(res => console.log("Result :", res.data))
                     .catch(err => console.log("Error :", err.data));
             }
         });
@@ -69,7 +88,7 @@ const subscription = web3.eth
                     .get(
                         `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${doc.telegramId}&parse_mode=MarkdownV2&text=*Debit*%0A%0ASender%20%3A%20${sender}%0A%0A*%5C-${amountText}%20${tokenLookUp[token]}*%0A%5BPolygonScan%5D(https%3A%2F%2Fpolygonscan.com%2Ftx%2F${tx}`
                     )
-                    .then(res => console.log("Result :", res.data))
+                    // .then(res => console.log("Result :", res.data))
                     .catch(err => console.log("Error :", err.data));
             }
         });
